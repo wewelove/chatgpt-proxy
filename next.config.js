f16810f30fd361dc2a83dbb4bc8561852773cce2
wewelove88 <wewelove88@gmail.com>
2023-06-21 16:12:48 +0800

    修改配置

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   async rewrites() {
-    return [
-      {
-        source: "/:slug*",
-        destination: "https://github.com/:slug*",
-      },
-    ];
+    return {
+      fallback: [
+        {
+          source: '/:path*',
+          destination: `https://github.com/:path*`,
+        },
+      ],
+    };
   },
 };
 
